375d58e6fca35713211209b0dbecc21149c4f775
Raghu Betina <raghu@firstdraft.com>
2019-05-16 09:21:23 -0500

    Fix ActiveAdmin

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+
   
+
+  # The routes below are for the ActiveAdmin dashboard located at /admin. You can ignore them.
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
 end
